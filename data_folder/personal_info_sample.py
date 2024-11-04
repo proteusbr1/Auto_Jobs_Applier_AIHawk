b633daf6ebc6a4981c799a79e5e8d76ee6b0635d
Mario Junqueira <drmariojunqueira@gmail.com>
2024-11-04 09:38:17 -0500

    personal_info_sample.py

---
+++ data_folder/personal_info_sample.py
@@ -0,0 +1,11 @@
+USER_RESUME_CHATGPT = """ 
+User Resume use in chatgpt
+"""
+
+USER_RESUME_SUMMARY = """
+Summary of the user resume 
+"""
+
+COMPANY_CAPABILITIES = """
+Company capabilities if looking for contract for your company
+"""
